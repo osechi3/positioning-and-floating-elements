cefd870050a6de2390508fe439cebe5626159d0d
osechi3 <holypoetwrk@gmail.com>
2020-11-13 08:17:11 +0300

    Making the sidebar responsive

--- src/app.js
+++ src/app.js
@@ -10,6 +10,8 @@ class Page {
     this.sectionLink = document.querySelector('.header__section-link')
     this.accountBtn = document.querySelector('.header__account-btn')
     this.shareBtn = document.querySelector('.article__share-btn')
+    this.articlesSidebar = document.querySelector('.articles-sidebar')
+    this.sidebarImages = document.querySelectorAll('.articles-sidebar__image')
 
     this.respond()
     this.initListeners()
@@ -23,6 +25,10 @@ class Page {
     console.log(window.innerWidth)
     if (window.innerWidth > 1000) {
       this.respondToNormalWidth()
+
+      if (window.innerWidth <= 1150) {
+        this.respondTo1150px()
+      }
     } else if (window.innerWidth <= 1000 && window.innerWidth > 650) {
       this.respondTo1000px()
     } else if (window.innerWidth <= 650) {
@@ -36,11 +42,22 @@ class Page {
     this.shareBtn.classList.remove('article__share-btn_hidden')
     this.sectionLink.classList.remove('header__section-link_hidden')
     this.searchBtn.classList.remove('header__search-btn_hidden')
+    this.sidebarImages.forEach(image => {
+      image.classList.remove('articles-sidebar__image_hidden')
+    })
+    this.articlesSidebar.classList.remove('articles-sidebar_hidden')
+  }
+
+  respondTo1150px () {
+    this.sidebarImages.forEach(image => {
+      image.classList.add('articles-sidebar__image_hidden')
+    })
   }
 
   respondTo1000px () {
     this.sectionLink.classList.add('header__section-link_hidden')
     this.searchBtn.classList.add('header__search-btn_hidden')
+    this.articlesSidebar.classList.add('articles-sidebar_hidden')
 
     this.accountBtn.classList.remove('header__account-btn_hidden')
     this.shareBtn.classList.remove('article__share-btn_hidden')
