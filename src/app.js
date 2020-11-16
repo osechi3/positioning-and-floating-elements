5921a1c135932758f546cf5e38f18daeb09e1a6f
osechi3 <holypoetwrk@gmail.com>
2020-11-16 11:48:46 +0300

    Removing fontawesome's icons of brands

--- src/app.js
+++ src/app.js
@@ -1,7 +1,6 @@
 import './styles/app.css'
 import 'normalize.css'
 import '@fortawesome/fontawesome-free/js/solid'
-import '@fortawesome/fontawesome-free/js/brands'
 import '@fortawesome/fontawesome-free/js/fontawesome'
 
 class Page {
